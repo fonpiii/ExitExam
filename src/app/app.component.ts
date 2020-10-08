5723e3244948870474bf07c37bcd141ca42a5ce0
Fonnajaja <60050222@kmitl.ac.th>
2020-10-08 22:02:13 +0700

    Set Starter project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,10 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'ExitExam';
-  public word = null;
-  public result: any[] = [];
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
 
-  }
-
-  checkMinLength() {
-    if (this.word.length < 20) {
-      Swal.fire(
-        'Information?',
-        'Messages smaller than 20',
-        'error'
-      )
-    }
-  }
-
-  submit() {
-    this.result = [];
-    this.appService.convertLowercase(this.word);
-    this.appService.replaceCharacter();
-    this.appService.splitWordWhiteSpace();
-    this.result = this.appService.convertToResult();
-    console.log(this.result);
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,63 +1,10 @@
 import { Injectable } from '@angular/core';
 
-export class Word {
-  lowercase: string;
-  character: string;
-  whitespace: any[];
-}
-
-export class WordFrequency {
-  word: string;
-  freq: number;
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-  private words: Word = {
-    lowercase: '',
-    character: '',
-    whitespace: []
-  };
-
-  private wordfrequency: WordFrequency[] = [];
 
   constructor() { }
 
-  convertLowercase(word: string) {
-    this.words.lowercase = null;
-    this.words.lowercase = word.toLowerCase();
-  }
-
-  replaceCharacter() {
-    this.words.character = null;
-    this.words.character = this.words.lowercase.split('?').join('');
-    this.words.character = this.words.character.split('!').join('');
-    this.words.character = this.words.character.split(',').join('');
-    console.log(this.words.character);
-  }
-
-  splitWordWhiteSpace() {
-    this.words.whitespace = null;
-    var whiteSpace = this.words.character.split(" ");
-    this.words.whitespace = whiteSpace;
-    console.log(this.words.whitespace);
-  }
-
-  convertToResult(): any {
-    this.wordfrequency = [];
-    for (let i = 0; i < this.words.whitespace.length; i++) {
-      var findData = this.wordfrequency.filter(x => x.word === this.words.whitespace[i]);
-      if (findData.length === 0) {
-        this.wordfrequency.push({word: this.words.whitespace[i], freq: 1});
-      }
-      else {
-        var findIndex = this.wordfrequency.findIndex(x => x.word === this.words.whitespace[i]);
-        this.wordfrequency[findIndex].freq++;
-      }
-    }
-    console.log(this.wordfrequency);
-    return this.wordfrequency;
-  }
 }
